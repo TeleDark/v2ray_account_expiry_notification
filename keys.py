b3c225cf22e84016320285de56d71c1053e15ced
TeleDark <46258401+TeleDark@users.noreply.github.com>
2024-06-02 12:22:56 +0330

    Update keys.py

--- keys.py
+++ keys.py
@@ -2,6 +2,7 @@ import os
 import yaml
 real_dir = os.path.dirname(os.path.realpath(__file__))
 json_file = os.path.join(real_dir,"accounts_info.json")
+login_file = os.path.join(real_dir,"login.py")
 config_yaml_file = os.path.join(real_dir, "./config.yaml")
 pickle_file = os.path.join(real_dir, "cookies.pkl")
 inactives_file = os.path.join(real_dir, "inactives.pkl")
